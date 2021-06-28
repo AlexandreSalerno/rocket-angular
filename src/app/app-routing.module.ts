abbf5041a71481921bb8971c30cc2a2acd9b637f
Guilherme-mendesc <guimendes1337@gmail.com>
2021-06-28 15:29:30 -0300

    adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobrenosComponent } from './sobrenos/sobrenos.component';
+ 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'entrar', pathMatch:'full'},
+
+  {path:'inicio',component: InicioComponent},
+
+  {path:'entrar',component: EntrarComponent},
+
+  {path:'sobrenos',component: SobrenosComponent}
+
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
