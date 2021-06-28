721acb6619a35e8c14af006736c497c6da83f84e
Guilherme-mendesc <guimendes1337@gmail.com>
2021-06-28 12:56:55 -0300

    quem somos nos

---
+++ src/app/quem-somos-nos/quem-somos-nos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quem-somos-nos',
+  templateUrl: './quem-somos-nos.component.html',
+  styleUrls: ['./quem-somos-nos.component.css']
+})
+export class QuemSomosNosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
