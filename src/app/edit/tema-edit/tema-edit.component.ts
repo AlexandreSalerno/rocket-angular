ab66a48a08c708b8c84887b1f119eebfe772c4a7
Guilherme-mendesc <guimendes1337@gmail.com>
2021-07-06 11:00:55 -0300

    implementação put-delete

--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from 'src/app/model/Tema';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-tema-edit',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaEditComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private temaService: TemaService
+
+  ) { }
+
+
+  ngOnInit() {
+
+    if (environment.token == '') {
+      this.router.navigate(['/sobrenos'])
+    }
+    this.temaService.refreshToken()
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdTema(id)
   }
 
+  findByIdTema(id: number) {
+    this.temaService.getById(id).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  atualizar() {
+    this.temaService.putTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema atualizado com sucesso!')
+      this.router.navigate(['/tema'])
+    })
+
+  }
+
+
+
 }
+
+  
+
+
+
