d870e3edf58375cb1361268e95f3d87bcfb33c7a
AlexandreSalerno <alexandre.salerno26@gmail.com>
2021-07-06 10:29:20 -0300

    Implementação do typescript do component 'tema'

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
+  listaTema: Tema[]
 
-  ngOnInit(): void {
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    if (environment.token == "") {
+      this.router.navigate(["/sobrenos"])
+    }
+
+    this.temaService.refreshToken()
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert("Tema cadastrado com sucesso!")
+      this.tema = new Tema()
+    })
+  }
+
+  findAllTema() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTema = resp
+    })
   }
 
 }
