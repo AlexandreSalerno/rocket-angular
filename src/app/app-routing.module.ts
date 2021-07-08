b90eea115d23e60e0cd6510dda8734ae9c80e632
AlexandreSalerno <alexandre.salerno26@gmail.com>
2021-07-08 11:44:07 -0300

    Layout da tela de editar postagens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostagensDeletComponent } from './delet/postagens-delet/postagens-delet.component';
 import { TemaDeletComponent } from './delet/tema-delet/tema-delet.component';
+import { PostagensEditComponent } from './edit/postagens-edit/postagens-edit.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
@@ -28,7 +30,11 @@ const routes: Routes = [
 
   {path:'tema-delet/:id', component: TemaDeletComponent},
 
-  {path:'postagens', component: PostagensComponent}
+  {path:'postagens', component: PostagensComponent},
+
+  {path:'postagens-edit/:id', component: PostagensEditComponent},
+
+  {path:'postagens-delet/:id', component: PostagensDeletComponent}
 
 ];
 
