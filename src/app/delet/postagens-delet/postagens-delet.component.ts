c30ad4ad108d45115fe5f96fe6ddcccf6b288f73
Guilherme-mendesc <guimendes1337@gmail.com>
2021-07-08 12:46:36 -0300

    front-delete postagens

--- src/app/delet/postagens-delet/postagens-delet.component.ts
+++ src/app/delet/postagens-delet/postagens-delet.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from 'src/app/model/Postagem';
+import { Tema } from 'src/app/model/Tema';
+import { TemaService } from 'src/app/service/tema.service';
 
 @Component({
   selector: 'app-postagens-delet',
   templateUrl: './postagens-delet.component.html',
   styleUrls: ['./postagens-delet.component.css']
 })
-export class PostagensDeletComponent implements OnInit {
-
-  constructor() { }
+export class PostagensDeletComponent implements OnInit { 
+  
+  
+  constructor( ) { }
 
   ngOnInit(): void {
+    
   }
 
+  deletePostagem(){}
 }
