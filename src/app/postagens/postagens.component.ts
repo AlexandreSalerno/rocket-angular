24b6399a1552a74175b9cd15dbf273d061e07b5b
GiovannaTorres <giovannacltorres@gmail.com>
2021-07-08 11:54:05 -0300

    postagens.ts

--- src/app/postagens/postagens.component.ts
+++ src/app/postagens/postagens.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagensService } from '../service/postagens.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-postagens',
@@ -7,9 +15,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagensComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
 
-  ngOnInit(): void {
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  user: User = new User()
+  idUser = environment.id
+
+  serie = environment.serie
+
+
+  constructor(
+
+    public auth: AuthService,
+    public router: Router,
+    private postagemService: PostagensService,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      this.router.navigate(['/sobrenos'])
+    }
+
+    this.getAllTemas()
+    this.getAllPostagens()
+    this.temaService.refreshToken()
+  }
+
+  
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getById(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  findByIdUser(){
+    this.postagemService.getByIdUser(this.idUser).subscribe((resp: User) => {
+      this.user = resp
+    })
+  }
+  
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.user.serie = this.serie
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+
+      alert('Sua postagem foi enviada!')
+
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
   }
 
 }
