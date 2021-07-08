662b8dcd5d035549de93e91c55f6fe0fb866cd0c
JoaeLuisCorreia <josecorreia199@gmail.com>
2021-07-08 09:39:02 -0300

    Componentes edit e delet de postagens criados

---
+++ src/app/edit/postagens-edit/postagens-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postagens-edit',
+  templateUrl: './postagens-edit.component.html',
+  styleUrls: ['./postagens-edit.component.css']
+})
+export class PostagensEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
